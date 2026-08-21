1257bb82b396925f5b0b57b2b693a33fd6482f62
Toluwalope Akinkunmi <akinkunmitolulope23@gmail.com>
2026-08-21 07:38:49 +0100

    feat(series): add Lawexa recording as Mini Series 6
    Titles the episode "Lawexa" and links the card to its recording.

--- components/series/SeriesClient.tsx
+++ components/series/SeriesClient.tsx
@@ -20,6 +20,10 @@ const miniSeries = Array.from({ length: 8 }, (_, i) => ({
   href: undefined as string | undefined,
 }));
 
+miniSeries[5].title = "Lawexa";
+miniSeries[5].href =
+  "https://www.linkedin.com/video/event/urn:li:ugcPost:7481685800481431552";
+
 miniSeries[6].title = "Case Rader";
 miniSeries[6].href =
   "https://www.linkedin.com/video/event/urn:li:ugcPost:7488884253838106626";
